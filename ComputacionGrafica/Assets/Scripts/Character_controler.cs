7fee9e1b122cf9646582f15db01f25754cd9e3d3
Dan802 <88262514+Dan802@users.noreply.github.com>
2022-02-07 14:12:14 -0500

    Defense
    VFX ready

--- ComputacionGrafica/Assets/Scripts/Character_controler.cs
+++ ComputacionGrafica/Assets/Scripts/Character_controler.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 
 public class Character_controler : MonoBehaviour
 {
-    //[SerializeField] private Rigidbody rigibodyNinja;
+    [SerializeField] private ParticleSystem Ps_Anticipacion;
+    [SerializeField] private ParticleSystem Ps_Escudo;
+    public GameObject Escudo;
+    public GameObject Anticipacion;
     Animator animator;
 
     // Start is called before the first frame update
@@ -22,6 +25,8 @@ public class Character_controler : MonoBehaviour
         bool iswalking = animator.GetBool("Walking");
         bool runPressed = Input.GetKey("left shift");
         bool isrunning = animator.GetBool("Running");
+        bool defensePressed = Input.GetKey("f");
+        bool isdefending = animator.GetBool("Defense");
 
         if (!iswalking && forwardPressed)
             animator.SetBool("Walking", true);
@@ -34,5 +39,27 @@ public class Character_controler : MonoBehaviour
 
         if (isrunning && (!forwardPressed || !runPressed))
             animator.SetBool("Running", false);
+
+        if (defensePressed && !isdefending)
+        {
+            animator.SetBool("Defense", true);
+
+            Ps_Anticipacion.Play();
+
+            if (!Ps_Anticipacion.IsAlive())
+            {
+                Debug.Log("activado");
+                Escudo.SetActive(true);
+                Ps_Escudo.Play();
+            }
+        }
+
+        
+
+        if (!defensePressed && isdefending)
+        {
+            animator.SetBool("Defense", false);
+            Escudo.SetActive(false);
+        }
     }
 }
