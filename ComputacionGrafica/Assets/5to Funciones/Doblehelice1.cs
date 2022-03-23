51dbaf3974165b9b8514357cbc6744afa63f344f
Dan802 <88262514+Dan802@users.noreply.github.com>
2022-03-23 16:54:21 -0500

    Actividad evaluación de funciones
    Listo :3

--- ComputacionGrafica/Assets/5to Funciones/Doblehelice1.cs
+++ ComputacionGrafica/Assets/5to Funciones/Doblehelice1.cs
@@ -3,27 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class CardioideScript1 : MonoBehaviour
+public class Doblehelice1 : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float travelSpeed = 2;
+    [SerializeField] private Transform target2;
+    [SerializeField] private float travelSpeed = 2.0f;
     [SerializeField] private Axis planeNormal = Axis.Y;
     [SerializeField] private float startAngle = 0;
     [SerializeField] private float radio = 2;
-    [SerializeField] private float altura = 0.1f;
-    private float contorno = 0;
+    [SerializeField] private float height = 0.7f;
+    private float angulo = 0;
+    
+    private TrailRenderer target1_tail;
+    private TrailRenderer target2_tail;
+    [SerializeField] private float tail_Lifetime = 10.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //contorno = Mathf.Deg2Rad * startAngle;
-        contorno = Mathf.Rad2Deg * startAngle;
+        angulo = Mathf.Rad2Deg * startAngle;
+
+        target1_tail = target.GetComponent<TrailRenderer>();
+        target2_tail = target.GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Doblehelice();
+        tailRender();
     }
 
     void Doblehelice()
@@ -34,20 +44,46 @@ public class CardioideScript1 : MonoBehaviour
         switch (planeNormal)
         {
             case Axis.X:
-                targetLocalVector.x = targetLocalVector.x + Time.deltaTime * altura;
-                targetLocalVector.Set(targetLocalVector.x, radio, contorno);
+                targetLocalVector.x = targetLocalVector.x + Time.deltaTime * height;
+                targetLocalVector.Set(targetLocalVector.x, radio, angulo);
                 break;
             case Axis.Y:
-                targetLocalVector.y = targetLocalVector.y + Time.deltaTime * altura;
-                targetLocalVector.Set(radio, targetLocalVector.y, contorno);
+                targetLocalVector.y = targetLocalVector.y + Time.deltaTime * height;
+                targetLocalVector.Set(radio, targetLocalVector.y, angulo);
                 break;
             case Axis.Z:
-                targetLocalVector.z = targetLocalVector.z + Time.deltaTime * altura;
-                targetLocalVector.Set(radio, contorno, targetLocalVector.z);
+                targetLocalVector.z = targetLocalVector.z + Time.deltaTime * height;
+                targetLocalVector.Set(radio, angulo, targetLocalVector.z);
                 break;
         }
         targetLocalVector.PolarToCartesianPlanar(planeNormal);
         target.localPosition = targetLocalVector;
-        contorno += Time.deltaTime * travelSpeed;
+        angulo += Time.deltaTime * travelSpeed;
+
+        //Target 2
+        Vector3 target2LocalVector = target2.localPosition;
+        target2LocalVector.CartesianToPolarPlanar(planeNormal);
+
+        switch (planeNormal)
+        {
+            case Axis.X:
+                target2LocalVector.Set(targetLocalVector.x, -radio, angulo);
+                break;
+            case Axis.Y:
+                target2LocalVector.Set(-radio, targetLocalVector.y, angulo);
+                break;
+            case Axis.Z:
+                target2LocalVector.Set(-radio, angulo, targetLocalVector.z);
+                break;
+        }
+        target2LocalVector.PolarToCartesianPlanar(planeNormal);
+        target2.localPosition = target2LocalVector;
+        angulo += Time.deltaTime * travelSpeed;
+    }
+
+    void tailRender ()
+    {
+        target1_tail.time = tail_Lifetime;
+        target2_tail.time = tail_Lifetime;
     }
 }
